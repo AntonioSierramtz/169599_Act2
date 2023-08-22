98d0d3ea5439bf2034b7ac33389e41139dba85b9
AntonioSierramtz <141789796+AntonioSierramtz@users.noreply.github.com>
2023-08-22 17:24:07 -0600

    Update Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -2,19 +2,17 @@ package FigurasGeometricas;
 
 public class Circulo {
 	public double radio;
+	public Circulo() {
+		this (169599);
+	}
 	public Circulo(double radio) {
 	this.radio = radio;
-
 	}
 	public double calculaAreaCirculo() {
 	return Math.PI* (radio*radio); 
 	}
 	public double calcularCircunferencia() {
 	return (Math.PI*2)*radio;
-	}
-
-	public Circulo() {
-		this (169599);
 	}
 	public double obtenerRadio() {
 		return radio;
